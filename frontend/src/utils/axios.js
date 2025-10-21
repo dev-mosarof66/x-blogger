af825152a6f11dbc23af665aacd4f8f549695074
mosarof66 <devmosarof.123@gmail.com>
2025-10-21 07:09:26 +0600

    render backend uri integrated with frontend

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3000'
+const mode = import.meta.env.VITE_DEV_MODE
+const url = mode === 'dev' ? 'http://localhost:3000' : import.meta.env.VITE_BACKEND_URI
+
 const axiosInnstance = axios.create({
-    baseURL:`${url}/api/v1`,
-    withCredentials:true
+    baseURL: `${url}/api/v1`,
+    withCredentials: true
 })
 
 export default axiosInnstance
