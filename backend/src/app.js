b7886176967c3d5b74d17f5bf44aed93b0b951d2
mosarof66 <devmosarof.123@gmail.com>
2025-10-21 07:24:05 +0600

    trying to fix cors issues

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,37 +10,30 @@ const app = express();
 app.use(helmet());
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://x-blogger.vercel.app",
-  "https://x-blogger-qpo3ewv6x-mosarof.vercel.app",
+    "https://x-blogger.vercel.app",
+    "https://x-blogger-qpo3ewv6x-mosarof.vercel.app",
+    "http://localhost:5173",
 ];
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+    cors({
+        origin: allowedOrigins,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
 );
 
 app.use(cookieParser());
 
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+    app.use(morgan("dev"));
 }
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: "Too many requests from this IP, please try again later",
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again later",
 });
 app.use(limiter);
 
@@ -50,7 +43,7 @@ app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(express.static("public"));
 
 app.get("/health-check", (req, res) => {
-  res.send("Blogging platform API is running...");
+    res.send("Blogging platform API is running...");
 });
 
 import userRoutes from "./routes/user.routes.js";
